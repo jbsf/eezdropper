b947985a7b38cd5f91e72dee23e1a2c796b73365
jbs <jb@precita-2.local>
2012-02-07 20:07:33 -0800

    Initial Commit

---
+++ eezdropper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  eezdropper
+//
+//  Created by JB Steadman on 2/7/12.
+//  Copyright (c) 2012 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ eezdropper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eezdropper
+//
+//  Created by JB Steadman on 2/7/12.
+//  Copyright (c) 2012 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
