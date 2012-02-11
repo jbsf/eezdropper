50022d081f838af5f288fa274390223a186d40c6
jbs <jb@precita-2.local>
2012-02-10 21:49:21 -0800

    Revert "Revert "pre-cedar""
    This reverts commit d8e2bad7646f458dc54506e83088b91ca3675142.

--- eezdropper/PeerWatcher.h
+++ eezdropper/PeerWatcher.h
@@ -3,9 +3,5 @@
 #import "PeerWatcherDelegate.h"
 
 @interface PeerWatcher : NSObject<GKSessionDelegate>
-
-@property (nonatomic, retain) GKSession *gkSession;
-@property (nonatomic, assign) id<PeerWatcherDelegate> peerWatcherDelegate;
-
 - (id)initWithDelegate:(id<PeerWatcherDelegate>)delegate;
 @end
